a6aab331e7eb0d2968b109bc6197640a4fdb95d1
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2023-08-20 21:17:33 -0400

    88. Lidando com Array de Portas #02

---
+++ functions/porta.ts
@@ -0,0 +1,23 @@
+import PortaModel from "../model/porta"
+
+//Por se tratar de um array de portas, o PortaModel deve ser iniciado com colchetes no final
+export function criarPortas(qtde: number, selecionada: number):PortaModel[] {
+    
+    //Essa funcao é para criar um array de elementos com objeto
+    return Array.from({length: qtde}, (_,i) => {
+        const numero = i + 1
+        const temPresente = numero === selecionada
+        return new PortaModel(numero, temPresente)
+    })
+}
+
+export function atualizarPortas(portas: PortaModel[], portaModificada: PortaModel): PortaModel[]{
+    return portas.map(portaAtual => {
+        const igualAModificada = portaAtual.numero === portaModificada.numero
+        if(igualAModificada){
+            return portaModificada
+        }else{
+            return portaModificada.aberta ? portaAtual : portaAtual.deselecionada()
+        }
+    })
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 
 import Presente from "../../components/Presente";
 import Porta from "../../components/Porta";
-import PortaModel from "../../model/porta";
-
+import {atualizarPortas, criarPortas} from "../../functions/porta"
 export default function Home() {
   //Dessa forma iremos criar a constante p1 como
-  const [p1, setP1] = useState(new PortaModel(1));
-  const [texto, setTexto] = useState("...")
+const[portas, setPortas] = useState(criarPortas(3,2))
+
+function renderizarPortas(){
+  return portas.map(porta => {
+    return <Porta key={porta.numero} value={porta} 
+    onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
+  })
+}
+  
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+    <div style={{ display: "flex"}}>
+
+      {renderizarPortas()}
       {/* <Presente /> */}
     </div>
   );
